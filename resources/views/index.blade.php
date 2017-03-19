fa03980e621d4513a17e78dd910bb36e936fef6d
IVAN-PC\Ivan <ichepanov@gmail.com>
2017-03-19 22:21:09 +0200

    initial commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+    <head>
+	    <meta charset="utf-8">
+	    <title>Index</title>
+	    <link rel="icon" type="/image/png" href="/img/favicon.png">
+	    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+        <!-- Bootstrap -->
+        <link href="/css/bootstrap.min.css" rel="stylesheet" media="screen">
+        <script src="/js/jquery.min.js"></script>
+        <script src="/js/bootstrap.min.js"></script>
+
+
+        <script src="/js/plugins/morris/morris.js"></script>
+        <script src="/js/plugins/morris/raphael-2.1.0.min.js"></script>
+        <link href="/css/plugins/morris/morris-0.4.3.min.css" rel="stylesheet">
+
+        <script src="/js/plugins/flot/jquery.flot.js"></script>
+        <script src="/js/plugins/flot/jquery.flot.tooltip.min.js"></script>
+        <script src="/js/plugins/flot/jquery.flot.resize.js"></script>
+        <script src="/js/plugins/flot/jquery.flot.time.js"></script>
+        <script src="/js/plugins/flot/jquery.flot.pie.js"></script>
+        <script src="/js/plugins/flot/curvedLines.js"></script>
+        <script src="/js/plugins/flot/excanvas.min.js"></script>
+        <script src="/js/plugins/flot/jquery.flot.spline.js"></script>
+        <script src="/js/plugins/flot/jquery.flot.symbol.js"></script>
+    </head>
+    <body class="page-index">
+
+    </body>
+</html>
