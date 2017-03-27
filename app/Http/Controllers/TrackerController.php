ad036f4b7e750598fa67315c86faa48634a624ea
IVAN-PC\Ivan <ichepanov@gmail.com>
2017-03-27 21:52:14 +0300

    finished

--- app/Http/Controllers/TrackerController.php
+++ app/Http/Controllers/TrackerController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Tracker;
 class TrackerController extends Controller
 {
     public function show(){
 
-        return view('index');
+        $pageViews = Tracker::pageViews(60 * 24 * 120);
+
+        $pageViewsPerCountry = Tracker::pageViewsByCountry(60 * 24 * 120);
+
+        $allData = [$pageViews,$pageViewsPerCountry];
+
+        return view('index', compact('crumbs','allData','selected_period'));
     }
 }
